226d9358fe1fc604da934f5d0c103c9e11cdd3c9
Matias Yocca <matias@yocca.dev>
2023-08-12 16:51:31 -0300

    feat: create orders model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+    ];
+
+    /**
+     * Get the user that owns the order.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
