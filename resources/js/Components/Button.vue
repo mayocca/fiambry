90ac1ccd6979187dc47958c23ce7c5baa74b1c55
Matias Yocca <matias@yocca.dev>
2023-08-26 22:35:03 -0300

    fix: move button style to its component

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="relative block w-full px-4 py-2 bg-white text-black shadow-md gradient-border hover:shadow-lg"
+        class="relative block w-full px-4 py-1 bg-white text-black shadow-md gradient-border hover:shadow-lg"
     >
         {{ title }}
     </button>
@@ -14,3 +14,21 @@ defineProps({
     },
 });
 </script>
+
+<style scoped lang="scss">
+$border-radius: 2px;
+
+.gradient-border {
+    border-radius: $border-radius;
+}
+.gradient-border::after {
+    $extra: 2px;
+    content: "";
+    position: absolute;
+    inset: -$extra;
+    z-index: -1;
+    background: linear-gradient(to bottom right, #3b82f6, #f59e0b, #ef4444);
+    background-clip: padding-box;
+    border-radius: $border-radius + $extra;
+}
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -18,21 +18,3 @@ export default {
     layout: Layout,
 };
 </script>
-
-<style scoped lang="scss">
-$border-radius: 2px;
-
-.gradient-border {
-    border-radius: $border-radius;
-}
-.gradient-border::after {
-    $extra: 2px;
-    content: "";
-    position: absolute;
-    inset: -$extra;
-    z-index: -1;
-    background: linear-gradient(to bottom right, #3b82f6, #f59e0b, #ef4444);
-    background-clip: padding-box;
-    border-radius: $border-radius + $extra;
-}
-</style>
