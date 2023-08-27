31fa60d1b82f6ebff026def5a77241a54f8e3bb2
Matias Yocca <5626046+mayocca@users.noreply.github.com>
2023-08-27 18:15:02 -0300

    feat: add allowed products seeding

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -15,9 +15,13 @@ class OrderSeeder extends Seeder
         \App\Models\Order::factory(10)
             ->create()
             ->each(function (\App\Models\Order $order) {
+                $allowedProducts = \App\Models\Product::all()->random(rand(3, 7));
+
+                $order->allowedProducts()->sync($allowedProducts);
+
                 for ($i = 0; $i < rand(3, 7); $i++) {
                     $order->products()->attach(
-                        \App\Models\Product::all()->random()->id,
+                        $allowedProducts->random(),
                         [
                             'user_id' => \App\Models\User::all()->random()->id,
                             'quantity' => rand(1, 5),
